d6e29da3283cc03cd3572a1a245b91ec458d4e6c
guoliang <guoliang@baiing.cn>
2020-11-16 11:35:30 +0800

    first release

---
+++ WXNSDK/WXNSDK/WXNSDK.h
@@ -0,0 +1,18 @@
+//
+//  WXNSDK.h
+//  WXNSDK
+//
+//  Created by Bright on 2020/11/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WXNSDK.
+FOUNDATION_EXPORT double WXNSDKVersionNumber;
+
+//! Project version string for WXNSDK.
+FOUNDATION_EXPORT const unsigned char WXNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WXNSDK/PublicHeader.h>
+
+
---
+++ WXNSDK/WXNSDK/WXNSDK.swift
@@ -0,0 +1,18 @@
+//
+//  WXNSDK.swift
+//  WXNSDK
+//
+//  Created by Bright on 2020/11/16.
+//
+
+import Foundation
+
+public class Say {
+    public func startSay() {
+        print("sayhello")
+    }
+    
+    private func endSay() {
+        print("sayBye")
+    }
+}
